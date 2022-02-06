7897712d664c3c65c434e973257d84ac68a7db9e
Joseph Wilson <joseph.wilson@optalysys.com>
2022-02-06 21:47:07 +0000

    Rewrote auth

--- source/_static/js/import.js
+++ source/_static/js/import.js
@@ -1,8 +1,13 @@
 import Amplify, {Auth} from 'aws-amplify';
 import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
-
 import {Hub, Logger} from 'aws-amplify';
+
+
+Amplify.configure({
+    aws_cognito_region: 'eu-west-1',
+    aws_user_pools_id:  "eu-west-1_QLZhzSObW", // (optional) -  Amazon Cognito User Pool ID   
+    aws_user_pools_web_client_id: "11e47qm43729i0b95k8lh1q8n6", // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
+});
 
 const logger = new Logger('FirstLogin');
 const listener = (data) => {
